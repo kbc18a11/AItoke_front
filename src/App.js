203dbcd67b99da8a1ef6e6e1d331dec31742bdd0
Naoumi1214 <wada121400@gmail.com>
2020-05-15 16:45:12 +0900

    update：それぞれメンバーの作業用領域のルーティング設定を行った

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
+import Wada from './pages/development/Wada';
+import Murase from './pages/development/Murase';
+import Tsutsui from './pages/development/Tsutsui';
+import Nakagoshi from './pages/development/Nakagoshi';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <Router>
+          <Switch>
+            <Route path="/wada" component={Wada} />
+            <Route path="/murase" component={Murase} />
+            <Route path="/tsutsui" component={Tsutsui} />
+            <Route path="/nakagoshi" component={Nakagoshi} />
+          </Switch>
+        </Router>
+      </div>
     </div>
   );
 }
