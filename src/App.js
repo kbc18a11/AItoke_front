89428303fab4a71333f00b133483e5679f98c1ec
Naoumi1214 <wada121400@gmail.com>
2020-05-15 16:51:01 +0900

    fix：インデント修正

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router,Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Wada from './pages/development/Wada';
 import Murase from './pages/development/Murase';
 import Tsutsui from './pages/development/Tsutsui';
