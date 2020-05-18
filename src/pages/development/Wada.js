8e202719b10f30401987df0e857a97b23ad12a98
Naoumi1214 <wada121400@gmail.com>
2020-05-18 15:18:18 +0900

    BootstrapReactのテストを行った

--- src/pages/development/Wada.js
+++ src/pages/development/Wada.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class Wada extends Component {
     render() {
         return (
             <div>
                 <h1>和田用</h1>
+                <Button variant="primary">Primary</Button>
             </div>
         )
     }
