0f1213a5022fcbcf80b11d1ecb35aad04e4fa8fd
Naoumi1214 <wada121400@gmail.com>
2020-06-05 15:47:31 +0900

    add : myMessageForm.js

---
+++ src/components/MyMessageForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class MyMessageForm extends Component {
+    render() {
+        return (
+            <div>
+                <p>aaaaaaa</p>
+            </div>
+        )
+    }
+}
--- src/pages/development/Wada.js
+++ src/pages/development/Wada.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react'
 import {Button} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MyMessageForm from '../../components/MyMessageForm';
 
 export default class Wada extends Component {
     render() {
         return (
             <div>
-                <h1>和田用</h1>
+                <MyMessageForm/>
                 <Button variant="primary">Primary</Button>
             </div>
         )
