31ec150a79676eee209a8d60ffd7a050cddfbfdd
Naoumi1214 <wada121400@gmail.com>
2020-06-06 17:48:08 +0900

    update : バリデーションの実装が完了した

--- src/components/MyMessageForm.js
+++ src/components/MyMessageForm.js
@@ -9,11 +9,37 @@ export default class MyMessageForm extends Component {
         super(props);
 
         this.state = {
+            //送信するメッセージの内容
             message: '',
         }
 
         this.doChange_message = this.doChange_message.bind(this);
+        this.validation = this.validation.bind(this);
+        this.doGetNobyAPI = this.doGetNobyAPI.bind(this);
     }
+
+    /**
+     * バリデーション
+     * 妥当性に問題が無ければ、false
+     */
+    validation() {
+        const errorList = [];//エラーメッセージのリスト
+        
+        //送信するメッセージは、空文字列か？
+        if (!this.state.message) {
+            errorList.push('メッセージがありません。')
+        }
+
+        //エラーメッセージは存在するか？
+        if (errorList.length > 0) {
+            alert(errorList);
+            return true;
+        }
+
+
+        return false;
+    }
+
 
     /**
      * messageの値の変更を受け付ける
@@ -21,11 +47,13 @@ export default class MyMessageForm extends Component {
      */
     doChange_message(e) {
         this.setState({ message: e.target.value });
-        //console.log(this.state.message);
     }
 
-    doGetNobyAPI(text) {
-        fetch(_URL, { text: text })
+    doGetNobyAPI(e) {
+        //バリデーションの検証
+        if (this.validation()) return;
+
+        fetch(_URL, { text: this.state.message })
             .then((res) => {
                 console.log(res);
             })
@@ -51,7 +79,7 @@ export default class MyMessageForm extends Component {
                             placeholder="メッセージを入力してください"
                             className="" onChange={this.doChange_message} /></Col>
                         <Col xs={6} md={3}><Button variant="primary" block className="">自分の声で喋る</Button></Col>
-                        <Col xs={6} md={3}><Button variant="primary" block className="">メッセージを届ける</Button></Col>
+                        <Col xs={6} md={3}><Button variant="primary" block className="" onClick={this.doGetNobyAPI}>メッセージを届ける</Button></Col>
                     </Row>
                 </Form>
             </Container>
