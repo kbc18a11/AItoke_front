5ec684e84a8ef8340f188c6e5763015f57cf6701
Naoumi1214 <wada121400@gmail.com>
2020-06-07 17:27:34 +0900

    fix : メソッド名の修正
    doGetNobyApi→doGetAPI

--- src/pages/development/Wada.js
+++ src/pages/development/Wada.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyMessageForm from '../../components/MyMessageForm';
 
 export default class Wada extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            voice: ''
+        }
+
+    }
+
+    setVoice(e) {
+        this.setState({ voiceText: e.voice });
+    }
+
     render() {
         return (
             <div>
                 <Button variant="primary">Primary</Button>
-                <MyMessageForm/>
+                <h1>{this.state.voice}</h1>
+                <MyMessageForm onChange={e => this.setVoice(e)}/>
             </div>
         )
     }
