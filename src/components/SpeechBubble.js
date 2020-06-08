b08e0c475a67c504efa6b80111df8610fabf3d85
Naoumi1214 <wada121400@gmail.com>
2020-06-08 16:01:38 +0900

    update : セリフを表示させることができた

--- src/components/SpeechBubble.js
+++ src/components/SpeechBubble.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { Button, Form, FormControl, Container, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class SpeechBubble extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            text: this.props.text
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log(nextProps);
+        
+        this.setState({ text: nextProps.text });
+    }
+
     render() {
         return (
             <div>
-                
+                <h1>{this.state.text}</h1>
             </div>
         )
     }
