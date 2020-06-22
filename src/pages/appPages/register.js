8f9f34ce50ce0eef8f5d85ec293584f07fe89f64
Naoumi1214 <wada121400@gmail.com>
2020-06-22 16:06:14 +0900

    add : /registerのページファイルを追加した

---
+++ src/pages/appPages/register.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Form, FormControl, Button, Container, Row } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class register extends Component {
+    render() {
+        return (
+            <Container>
+                <h1>ユーザー登録</h1>
+                <Row>
+
+                </Row>
+            </Container>
+        )
+    }
+}
