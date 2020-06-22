e25992b86a71dc81b6bfe0ec749d60e7699ef04c
Naoumi1214 <wada121400@gmail.com>
2020-06-22 16:10:28 +0900

    update : ユーザー登録(/rigister)を項目に追加

--- src/components/navber/DefaultNavigationbar.js
+++ src/components/navber/DefaultNavigationbar.js
@@ -12,6 +12,7 @@ export default class DefaultNavigationbar extends Component {
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="/register">ユーザー登録</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
