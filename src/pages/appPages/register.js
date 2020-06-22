a687d09dd6cdb6616776d5c4bfc614738800d3c1
Naoumi1214 <wada121400@gmail.com>
2020-06-22 16:09:36 +0900

    update : cssを追加

--- src/pages/appPages/register.js
+++ src/pages/appPages/register.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Form, FormControl, Button, Container, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../css/container.css';
 
 export default class register extends Component {
     render() {
