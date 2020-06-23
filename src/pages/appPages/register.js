243664c6f948b54ca0d9cd1b6b4df825d3cb188d
Naoumi1214 <wada121400@gmail.com>
2020-06-23 13:39:58 +0900

    update : ユーザー登録画面のデザイン途中

--- src/pages/appPages/register.js
+++ src/pages/appPages/register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Form, FormControl, Button, Container, Row } from 'react-bootstrap';
+import { Form, FormControl, Button, Container, Row,Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../css/container.css';
 
@@ -7,9 +7,28 @@ export default class register extends Component {
     render() {
         return (
             <Container>
-                <h1>ユーザー登録</h1>
                 <Row>
-
+                    <Col>
+                        <Form>
+                            <Form.Group controlId="formBasicEmail">
+                                <Form.Label>Email address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter email" />
+                                <Form.Text className="text-muted">
+                                    We'll never share your email with anyone else.
+                            </Form.Text>
+                            </Form.Group>
+                            <Form.Group controlId="formBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Password" />
+                            </Form.Group>
+                            <Form.Group controlId="formBasicCheckbox">
+                                <Form.Check type="checkbox" label="Check me out" />
+                            </Form.Group>
+                            <Button variant="primary" type="submit">
+                                Submit
+                        </Button>
+                        </Form>
+                    </Col>
                 </Row>
             </Container>
         )
