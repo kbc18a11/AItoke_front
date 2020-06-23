3d9e76d2292ae958785741de3643aa8c2166e8a0
Naoumi1214 <wada121400@gmail.com>
2020-06-23 14:18:55 +0900

    update : Live2DControllerを子コンポーネントにした

--- src/pages/appPages/Talking.js
+++ src/pages/appPages/Talking.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Button, Row, Col } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyMessageForm from '../../components/talk/MyMessageForm';
 import SpeechBubble from '../../components/talk/SpeechBubble';
 import TalkingLog from '../../components/talk/TalkingLog';
+import Live2DController from '../../components/talk/Live2DController';
 import '../../css/myMessageForm.css';
 
 export default class Talking extends Component {
@@ -11,8 +12,8 @@ export default class Talking extends Component {
         super(props);
 
         this.state = {
-            voiceText: '　',
-            log: { who: '', text: '' }
+            voiceText: '　',//ユーザーとAIのじゃべった内容
+            log: { who: '', text: '' }//ログ1行分
         }
 
     }
@@ -39,7 +40,7 @@ export default class Talking extends Component {
     render() {
         return (
             <Row>
-                <Col md={4}><h1>{this.state.voiceText}</h1></Col>
+                <Col md={4}><Live2DController modelSpeech={this.state.log.who === 'AI' ? this.state.log.text : ''} /></Col>
                 <Col md={8}>
                     <SpeechBubble text={this.state.voiceText} />
                     <MyMessageForm setVoice={e => this.setVoice(e)} setLog={e => this.setLog(e)} />
