7810a4c4105f3cc1dcf7f98949b89b996758b125
Naoumi1214 <wada121400@gmail.com>
2020-06-23 14:18:10 +0900

    fix : 画像の大きさを調整した

--- src/components/talk/Live2DController.js
+++ src/components/talk/Live2DController.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import character0 from './../../image/character0.png';
 import character1 from './../../image/character1.png';
+import '../../css/characterFace.css';
 
 export default class Live2DController extends Component {
 
@@ -18,7 +19,7 @@ export default class Live2DController extends Component {
 
 
     speaking() {
-        var synthes = new SpeechSynthesisUtterance();
+        const synthes = new SpeechSynthesisUtterance();
         synthes.voiceURI = 'native';
         synthes.volume = 1.0; //音量 min 0 ~ max 1
         synthes.rate = 1.0;   //速度 min 0 ~ max 10
@@ -26,9 +27,11 @@ export default class Live2DController extends Component {
         synthes.lang = 'ja-UP';
         synthes.text = this.state.modelSpeech;
         speechSynthesis.speak(synthes);
+        
         synthes.onstart = function () {
             document.getElementById('character').src = character1;
         }
+        
         synthes.onend = function () {
             document.getElementById('character').src = character0;
         }
