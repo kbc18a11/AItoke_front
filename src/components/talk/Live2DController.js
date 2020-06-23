395d8e41c6a90ba93ca68fd777dbbadd4f2359b5
Naoumi1214 <wada121400@gmail.com>
2020-06-23 13:44:41 +0900

    add : MRSのファイル追加した

---
+++ src/components/talk/Live2DController.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import character0 from './../../image/character0.png';
+import character1 from './../../image/character1.png';
+
+export default class Live2DController extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            modelSpeech: ''
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ modelSpeech: nextProps.modelSpeech });
+        this.speaking();
+    }
+
+
+    speaking() {
+        var synthes = new SpeechSynthesisUtterance();
+        synthes.voiceURI = 'native';
+        synthes.volume = 1.0; //音量 min 0 ~ max 1
+        synthes.rate = 1.0;   //速度 min 0 ~ max 10
+        synthes.pitch = 1.0;　//音程 min 0 ~ max 2
+        synthes.lang = 'ja-UP';
+        synthes.text = this.state.modelSpeech;
+        speechSynthesis.speak(synthes);
+        synthes.onstart = function () {
+            document.getElementById('character').src = character1;
+        }
+        synthes.onend = function () {
+            document.getElementById('character').src = character0;
+        }
+    }
+
+    render() {//描写処理
+        return (
+            <div>
+                <img id='character' src={character0} alt='' />
+            </div>
+        );
+    }
+}
