862339697d0502abf1ab54be97022b380582d17a
kbc18a11 <wada121400@gmail.com>
2020-07-03 12:09:17 +0900

    add : ユーザーデータ関係のFluxのViweファイルを追加

---
+++ src/flux/user/userViwe/Navber.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import DefaultNavigationbar from '../../../components/navber/DefaultNavigationbar';
+
+export default class Navber extends Component {
+    render() {
+        return (
+        );
+    }
+}
