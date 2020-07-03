5925e90ab9519cf307cb6a48e10059381526c4fe
kbc18a11 <wada121400@gmail.com>
2020-07-03 12:29:21 +0900

    add : ログイン中のナビゲーションバーを追加

---
+++ src/components/navber/NowLoginNavigationbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import UserNavMenu from './UserNavMenu';
+export default class NowLoginNavigationbar extends Component {
+    render() {
+        return (
+            <Navbar bg="primary" expand="lg" variant="dark">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <Nav.Link href="/register">ユーザー登録</Nav.Link>
+                    <UserNavMenu/>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-light">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
---
+++ src/components/navber/UserNavMenu.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import UserStore from '../../flux/user/UserStore';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+export default class UserNavMenu extends Component {
+    render() {
+        return (
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">ログアウト</NavDropdown.Item>
+            </NavDropdown>
+        )
+    }
+}
