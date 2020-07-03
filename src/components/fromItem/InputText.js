eac363154efcf65a839f4623fab1f34e2417e04b
kbc18a11 <wada121400@gmail.com>
2020-07-03 16:20:42 +0900

    update : 汎用的なform用のtextが完成した

--- src/components/fromItem/InputText.js
+++ src/components/fromItem/InputText.js
@@ -1,23 +1,32 @@
 import React, { Component } from 'react'
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
+import '../../css/errorText.css';
 
 export default class InputText extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            label: props.label,
-            type: props.type,
-            
+            label: props.label,//Form.Labelの表示文字
+            type: props.type,//Form.Control type
+            placeholder: props.placeholder,//Form.Control placeholder
+            outPutErrotMeaagages: props.outPutErrotMeaagages,//表示するバリデーションエラーメッセージ
+            rules: props.rules,//バリデーションルール
+            setValue: props.setValue,//親のプロパティに対するセッター
         }
+
     }
-    
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ outPutErrotMeaagages: nextProps.outPutErrotMeaagages });
+    }
+
     render() {
         return (
-            <Form.Group className="name">
-                <Form.Label>{}</Form.Label>
-                <Form.Control type="text" placeholder="名前を入力" onChange={this.setName} />
-                <Form.Text className="error">{this.state.erroeMessages.name}</Form.Text>
+            <Form.Group>
+                <Form.Label>{this.state.label}</Form.Label>
+                <Form.Control type={this.state.type} placeholder={this.state.placeholder} onChange={this.state.setValue} />
+                <Form.Text className="error">{this.state.outPutErrotMeaagages}</Form.Text>
             </Form.Group>
         )
     }
