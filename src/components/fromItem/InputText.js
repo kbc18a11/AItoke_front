0e77decb4d0764fcef93176a0b5cd2da2e25f474
kbc18a11 <wada121400@gmail.com>
2020-07-04 18:32:02 +0900

    fix : エラーメッセージの表示の仕組みを変更

--- src/components/fromItem/InputText.js
+++ src/components/fromItem/InputText.js
@@ -6,18 +6,30 @@ export default class InputText extends Component {
     constructor(props) {
         super(props);
 
+        let outPutErrotMeaagages = [];
+        //props.outPutErrotMeaagagesは存在しているか？
+        if (props.outPutErrotMeaagages) {
+            outPutErrotMeaagages = props.outPutErrotMeaagages
+        }
+
         this.state = {
             label: props.label,//Form.Labelの表示文字
             type: props.type,//Form.Control type
             placeholder: props.placeholder,//Form.Control placeholder
-            outPutErrotMeaagages: props.outPutErrotMeaagages,//表示するバリデーションエラーメッセージ
+            outPutErrotMeaagages: outPutErrotMeaagages,//表示するバリデーションエラーメッセージ
             setValue: props.setValue,//親のプロパティに対するセッター
         }
 
     }
 
     componentWillReceiveProps(nextProps) {
-        this.setState({ outPutErrotMeaagages: nextProps.outPutErrotMeaagages });
+        //nextProps.outPutErrotMeaagagesは存在しているか？
+        if (nextProps.outPutErrotMeaagages) {
+            this.setState({ outPutErrotMeaagages: nextProps.outPutErrotMeaagages });
+            return;
+        }
+
+        this.setState({ outPutErrotMeaagages: [] });
     }
 
     render() {
@@ -25,7 +37,7 @@ export default class InputText extends Component {
             <Form.Group>
                 <Form.Label>{this.state.label}</Form.Label>
                 <Form.Control type={this.state.type} placeholder={this.state.placeholder} onChange={this.state.setValue} />
-                <Form.Text className="error">{this.state.outPutErrotMeaagages}</Form.Text>
+                <Form.Text className="error">{this.state.outPutErrotMeaagages.toString()}</Form.Text>
             </Form.Group>
         )
     }
