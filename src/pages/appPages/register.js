a6e83f0b9adaf63cdd10827b85ca243bc49310ec
kbc18a11 <wada121400@gmail.com>
2020-07-04 18:32:17 +0900

    update : バリデーションの実装が完了した

--- src/pages/appPages/register.js
+++ src/pages/appPages/register.js
@@ -6,8 +6,7 @@ import '../../css/container.css';
 import '../../css/errorText.css';
 import axios from 'axios';
 import { _URL } from '../../apiURL/AITalk_outApiCall_and_Auth';
-import Validator from 'validatorjs';
-
+import ValidationManager from '../../modules/class/ValidationManager';
 
 export default class Register extends Component {
 
@@ -17,25 +16,29 @@ export default class Register extends Component {
         //hogehogeNumber -> errorMessagesの格納場所
         this.state = {
             name: '',
-            nameErrorNumber: 0,
             email: '',
-            emailErrorNumber: 0,
             password: '',
-            passwordErrorNumber: 0,
             //パスワードの確認
             password_confirmation: '',
-            password_confirmationErrorNumber: 0,
             //fromの項目ごとのバリデーションルール
             rules: {
                 name: 'required|max:255',
-                email: ''
+                email: 'required|email|max:255',
+                password: 'required|min:8|max:255'
             },
             //項目ごとのエラーメッセージ
-            errorMessages: ['', '', '', ''],
+            errorMessages: {
+                name: [],
+                email: [],
+                password: [],
+                password_confirmation: []
+            },
             //バリデーションルールごとのエラーメッセージ
             ruleTypeErrorMessages: {
-                required: '必須項目です。',
+                required: '必須項目です',
                 max: '255文字以下入力してください',
+                min: '8文字以上入力してください',
+                email:'メールアドレスを入力してください'
             },
         };
 
@@ -62,55 +65,51 @@ export default class Register extends Component {
         this.setState({ password_confirmation: e.target.value });
     }
 
+
     /**
-     * バリデーションチェック
-     * @param {string} targetKey //バリデーション対象の値の名前
-     * @param {string} targetValue //バリデーションの値
-     * @param {string} rule //バリデーションのルール
-     * @param {number} errorMessagesNumber //エラーメッセージを格納させる対象配列の添え字
+     * パスワードの入力確認チェック
+     * @returns {object} //エラーの結果
      */
-    checkValidation(targetKey, targetValue, rules, errorMessagesNumber) {
-        //console.log({ [targetKey]: targetValue });
-        //console.log(rules);
-
-
-        //バリデーションを検証
-        const validation = new Validator(
-            { [targetKey]: targetValue },
-            { [targetKey]: rules },
-            this.state.ruleTypeErrorMessages
-        );
-        console.log(validation.fails(), targetValue);
-
-        //バリデーションエラーはあるか？
-        if (validation.fails()) {
-            console.log(validation.errors.all());
-
-            //配列をコピーして、エラーメッセージを格納
-            const errorMessages_copy = this.state.errorMessages.slice();
-            errorMessages_copy[errorMessagesNumber] = validation.errors.all().undefined;
-            this.setState({ errorMessages: errorMessages_copy });
-
-            return false;
+    checktPassword_confirmation() {
+        //パスワードとパスワードの入力確認は、同じ値か？
+        if (this.state.password === this.state.password_confirmation) {
+            return { error: false };
         }
 
-        //バリデーションエラーはなかった
-        return true;
+        //エラー本文を返す
+        return { error: true, message: 'パスワードが違います' };
     }
-
-
 
     /**
      * 全てのバリデーションを実施する
+     * @returns {boolean} //バリデーションの結果
      */
     doValidation() {
-        //それぞれのバリデーションのパターンにあってるか結果を返す    
-        return this.checkValidation('name', this.state.name, this.state.rules.name, this.state.nameErrorNumber);
-        /** 
-            this.checkEmail() &
-            this.checkPassword() &&
-            this.checkPasswordConfirmation();
-            */
+        //checkValidationデータの対象
+        const targetData = {
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password,
+        };
+        const validationManager = new ValidationManager();
+
+        //setState用にthis.state.errorMessagesをコピー
+        let errorMessagesCopy = Object.assign({}, this.state.errorMessages);
+        //新しいエラーを返す
+        console.log(validationManager.checkValidation(targetData, this.state.rules, this.state.ruleTypeErrorMessages));
+        errorMessagesCopy = validationManager.checkValidation(targetData, this.state.rules, this.state.ruleTypeErrorMessages);
+
+        //this.state.password_confirmationのバリデーション
+        const resultObject = this.checktPassword_confirmation();
+        //パスワード入力確認ができてなかったか？
+        if (resultObject.error) {
+            //エラーの存在があったため、trueにする
+            validationManager.isError = true;
+            errorMessagesCopy.password_confirmation = resultObject.message;
+        }
+        this.setState({ errorMessages: errorMessagesCopy })
+        //最終的にエラーがあったか？
+        return validationManager.isError;
     }
 
     async requestRegister() {
@@ -166,11 +165,17 @@ export default class Register extends Component {
                     <Col md={{ span: 6, offset: 3 }}>
                         <Form>
                             <InputText className="name" label="名前" type="text"
-                                placeholder="名前を入力" outPutErrotMeaagages={this.state.errorMessages[this.state.nameErrorNumber]}
+                                placeholder="名前を入力" outPutErrotMeaagages={this.state.errorMessages.name}
                                 setValue={this.setName} />
-                            <InputText className="email" label="メールアドレス" type="text"
-                                placeholder="メールアドレスを入力" outPutErrotMeaagages={this.state.errorMessages[this.state.emailErrorNumber]}
+                            <InputText className="email" label="メールアドレス" type="email"
+                                placeholder="メールアドレスを入力" outPutErrotMeaagages={this.state.errorMessages.email}
                                 setValue={this.setEmail} />
+                            <InputText className="password" label="パスワード" type="password"
+                                placeholder="パスワードを入力" outPutErrotMeaagages={this.state.errorMessages.password}
+                                setValue={this.setPassword} />
+                            <InputText className="password_confirmation" label="パスワードの再入力" type="password"
+                                placeholder="パスワードを再入力" outPutErrotMeaagages={this.state.errorMessages.password_confirmation}
+                                setValue={this.setPassword_confirmation} />
                             <Button variant="primary" onClick={this.doSubmit}>登録</Button>
                         </Form>
                     </Col>
