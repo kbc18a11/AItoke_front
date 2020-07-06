b3674e99087a8b38468345da34a7f6e6bbbeae68
kbc18a11 <wada121400@gmail.com>
2020-07-06 15:30:42 +0900

    update : フォームの入力実装が完了した

--- src/pages/appPages/Login.js
+++ src/pages/appPages/Login.js
@@ -12,6 +12,26 @@ import userStore from '../../flux/user/UserStore';
 import { Redirect } from "react-router-dom";
 
 export default class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        };
+
+        this.setEmail = this.setEmail.bind(this);
+        this.setPassword = this.setPassword.bind(this);
+    }
+
+    setEmail(e) {
+        this.setState({ email: e.target.value });
+    }
+
+    setPassword(e) {
+        this.setState({ password: e.target.value });
+    }
+
     render() {
         return (
             <Container>
@@ -21,10 +41,10 @@ export default class Login extends Component {
                         <Form>
                             <InputText className="email" label="メールアドレス" type="email"
                                 placeholder="メールアドレスを入力" outPutErrotMeaagages={''}
-                                setValue={''} />
+                                setValue={this.setEmail} />
                             <InputText className="password" label="パスワード" type="password"
                                 placeholder="パスワードを入力" outPutErrotMeaagages={''}
-                                setValue={''} />
+                                setValue={this.setPassword} />
                             <Button variant="primary" onClick={this.doSubmit}>登録</Button>
                         </Form>
                     </Col>
