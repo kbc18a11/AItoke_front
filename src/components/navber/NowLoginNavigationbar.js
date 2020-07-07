33a1133965efb3f1be1871876fb7498d2a7aa67b
kbc18a11 <wada121400@gmail.com>
2020-07-07 12:18:49 +0900

    update : ナビゲーションバーの見出しの文章を変更

--- src/components/navber/NowLoginNavigationbar.js
+++ src/components/navber/NowLoginNavigationbar.js
@@ -9,7 +9,7 @@ export default class NowLoginNavigationbar extends Component {
     render() {
         return (
             <Navbar bg="primary" expand="lg" variant="dark">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="#home">AIトーク！！</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
