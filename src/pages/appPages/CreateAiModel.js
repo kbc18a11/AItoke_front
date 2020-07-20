df06a517441adaf4db93cda21808743ca3668233
kbc18a11 <wada121400@gmail.com>
2020-07-20 16:09:40 +0900

    add : ページファイルを追加 #11

---
+++ src/pages/appPages/CreateAiModel.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CreateAiModel extends Component {
+    render() {
+        return (
+            <div>
+                <h1>AIモデル登録</h1>
+            </div>
+        )
+    }
+}
