7f0de5bb8c9625fe70aae252267c9ccdb5592b1d
kbc18a11 <wada121400@gmail.com>
2020-08-02 22:29:00 +0900

    add : /userl/:id/aimodeのページを追加

---
+++ src/pages/appPages/UserCreatedAimodels.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export default class UserCreatedAimodels extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                a
+            </div>
+        )
+    }
+}
