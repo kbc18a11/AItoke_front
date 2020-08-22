c7551bfce32ed5422a6d2ae26c489d143e8b1917
kbc18a11 <wada121400@gmail.com>
2020-08-22 17:44:23 +0900

    update : コメントリストを追加

--- src/pages/appPages/AimodelInfo.js
+++ src/pages/appPages/AimodelInfo.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import AimodelProfile from '../../components/aiModel/AimodelProfile';
+import CommentList from '../../components/comment/CommentList.tsx';
 
 export default class AimodelInfo extends Component {
     constructor(props) {
@@ -15,6 +16,7 @@ export default class AimodelInfo extends Component {
         return (
             <div>
                 <AimodelProfile id={this.state.aimodel_id} />
+                <CommentList aimodel_id={this.state.aimodel_id} />
             </div>
         )
     }
